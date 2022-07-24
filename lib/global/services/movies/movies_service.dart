841b98d0e3c273628a7e5d5980956f9b26becfd8
umutarpat <arptumut@gmail.com>
2022-07-24 15:02:15 +0300

    added futurebuilder to search_movies_view updated search_movies_controller with multiple data fetching structure

--- lib/global/services/movies/movies_service.dart
+++ lib/global/services/movies/movies_service.dart
@@ -1,16 +1,19 @@
 import 'package:flutter_movie_app/global/interceptors/dio/dio_config.dart';
 import 'package:flutter_movie_app/global/interceptors/dio/get_dio.dart';
+import 'package:flutter_movie_app/global/models/response_data/search_movie_response_data/search_movie_response_data.dart';
 import 'package:flutter_movie_app/global/utils/logger.dart';
 
-Future searchMovieService() async {
+Future<SearchMovieResponseData> searchMovieService() async {
   try {
     var response = await getDio(dioOptions).get(
       "/search/movie",
     );
 
-    return response;
+    final movies = SearchMovieResponseData.fromJson(response.data);
+
+    return movies;
   } catch (e) {
     logger(e.toString());
-    return e;
+    return SearchMovieResponseData();
   }
 }
--- lib/modules/movies/pages/search_movies/search_movies_view.dart
+++ lib/modules/movies/pages/search_movies/search_movies_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_movie_app/modules/movies/pages/search_movies/controllers/search_movies_controller.dart';
+import 'package:get/get.dart';
 
 class MoviesSearchView extends StatelessWidget {
   const MoviesSearchView({Key? key}) : super(key: key);
@@ -6,7 +9,38 @@ class MoviesSearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: SingleChildScrollView(
+            child: GetBuilder<MoviesSearchViewController>(
+                id: 'main',
+                builder: (controller) {
+                  return FormBuilder(
+                      key: controller.formkey.value,
+                      child: Column(
+                        children: [
+                          FutureBuilder(
+                              future: controller.mainFuture.value,
+                              builder: (context, snapshot) {
+                                switch (snapshot.connectionState) {
+                                  case ConnectionState.active:
+                                  case ConnectionState.waiting:
+                                    return SizedBox(
+                                        height: 600,
+                                        child: CircularProgressIndicator());
+                                  case ConnectionState.done:
+                                    if (snapshot.hasError) {
+                                      return Container();
+                                    }
+                                    controller.assignFutures(
+                                        (snapshot.data! as List));
+                                    return Container();
+                                  default:
+                                    return SizedBox(
+                                        height: 600,
+                                        child: CircularProgressIndicator());
+                                }
+                              })
+                        ],
+                      ));
+                })));
   }
 }
